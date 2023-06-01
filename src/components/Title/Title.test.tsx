1023a194f4e5c8bd6b9039a9e519c17e77d4c761
grJDh <rusmukhametov@gmail.com>
2023-06-01 15:13:12 +0300

    Updated Title test

--- src/components/Title/Title.test.tsx
+++ src/components/Title/Title.test.tsx
@@ -3,8 +3,8 @@ import Title from "./Title";
 import { describe, expect, it } from "vitest";
 import { BrowserRouter } from "react-router-dom";
 
-describe("#App", () => {
-  it("renders App", async () => {
+describe("#Title", () => {
+  it("renders Title component", async () => {
     render(<Title />, { wrapper: BrowserRouter });
     const headerElement = screen.getByRole("banner");
     const titleElement = screen.getByText(/YouTube Graveyard/i);
@@ -17,6 +17,11 @@ describe("#App", () => {
     expect(linkElement).toBeInTheDocument();
     expect(linkElement).toHaveAttribute("href", "/");
     expect(linkElement).toHaveAttribute("aria-label", "Return to the Start");
+  });
+
+  it("matches snapshot", () => {
+    render(<Title />, { wrapper: BrowserRouter });
+    const headerElement = screen.getByRole("banner");
 
     expect(headerElement).toMatchSnapshot();
   });
