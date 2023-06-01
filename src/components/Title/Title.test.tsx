681d9dad0f9debee1f0b672f566b7e18574f36a3
grJDh <rusmukhametov@gmail.com>
2023-06-01 15:42:28 +0300

    Added final tests for Title component

--- src/components/Title/Title.test.tsx
+++ src/components/Title/Title.test.tsx
@@ -7,13 +7,17 @@ describe("#Title", () => {
   it("renders Title component", async () => {
     render(<Title />, { wrapper: BrowserRouter });
     const headerElement = screen.getByRole("banner");
-    const titleElement = screen.getByText(/YouTube Graveyard/i);
-    const logoElement = screen.getByAltText(/logo/i);
+    const titleElement = screen.getByRole("heading");
+    const logoElement = screen.getByRole("img");
     const linkElement = screen.getByRole("link");
 
     expect(headerElement).toBeInTheDocument();
     expect(titleElement).toBeInTheDocument();
+
     expect(logoElement).toBeInTheDocument();
+    expect(logoElement).toHaveAttribute("alt", "Logo");
+    expect(logoElement).toHaveAttribute("src", "/youtube-graveyard/src/assets/grave_main.svg");
+
     expect(linkElement).toBeInTheDocument();
     expect(linkElement).toHaveAttribute("href", "/");
     expect(linkElement).toHaveAttribute("aria-label", "Return to the Start");
@@ -24,5 +28,19 @@ describe("#Title", () => {
     const headerElement = screen.getByRole("banner");
 
     expect(headerElement).toMatchSnapshot();
+  });
+
+  it("renders different text with text prop given", () => {
+    render(<Title text="Test" />, { wrapper: BrowserRouter });
+    const titleElement = screen.getByRole("heading");
+
+    expect(titleElement).toHaveTextContent("Test");
+  });
+
+  it("renders coffin with coffin prop given", () => {
+    render(<Title coffin />, { wrapper: BrowserRouter });
+    const logoElement = screen.getByRole("img");
+
+    expect(logoElement).toHaveAttribute("src", "/youtube-graveyard/src/assets/coffin_error.svg");
   });
 });
